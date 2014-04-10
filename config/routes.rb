98d01483f55cba254534152838edf5da6d3e4c12
JakeCataford <JakeCataford@Gmail.com>
2014-04-10 11:21:55 -0400

    Adds Dashboard controller and views

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Progress::Application.routes.draw do
+  get "dashboard/index"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
