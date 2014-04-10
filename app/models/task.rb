229abe7d8310370f77b9ba9278ebda2ec12ea459
JakeCataford <JakeCataford@Gmail.com>
2014-04-10 10:41:02 -0400

    Adds more robust asociations to models

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,6 +1,6 @@
 class Task < ActiveRecord::Base
-  belongs_to :user, foreign_key: :owner
-  belongs_to :user, foreign_key: :requester
+  belongs_to :owner, class_name: 'User', foreign_key: :owner
+  belongs_to :requester, class_name: 'User', foreign_key: :requester
   belongs_to :project, foreign_key: :project
   has_ancestry
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ActiveRecord::Base
-  has_many :tasks, foreign_key: :owner
-  has_many :tasks, foreign_key: :requester
+  has_many :owned_tasks, class_name: 'Task',foreign_key: :owner
+  has_many :requested_tasks, class_name: 'Task', foreign_key: :requester
   validates :email, format: /\A(|(([A-Za-z0-9]+_+)|([A-Za-z0-9]+\-+)|([A-Za-z0-9]+\.+)|([A-Za-z0-9]+\++))*[A-Za-z0-9]+@((\w+\-+)|(\w+\.))*\w{1,63}\.[a-zA-Z]{2,6})\z/i
   validates :name, presence: true
 end
